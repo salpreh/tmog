4c2f8245248e5fe954febd0c47493e30621fead6
Salva Pérez <sperher@mercadona.es>
2024-02-11 21:38:22 +0100

    tmog2 solution

--- tmog-002/src/main/java/org/tfoc/Solution.java
+++ tmog-002/src/main/java/org/tfoc/Solution.java
@@ -1,13 +1,43 @@
 package org.tfoc;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public boolean isValid(String s) {
+    private static final int NONE = 0;
+    private static final int PARENTHESES = 1;
+    private static final int BRACKET = 2;
+    private static final int SQUARE = 3;
 
-        return false;
+    public static boolean isValid(String s) {
+        Deque<Integer> parenthesesStack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (isOpeningParentheses(c)) parenthesesStack.push(getParenthesesType(c));
+            else if (isClosingParentheses(c) && (parenthesesStack.isEmpty() || getParenthesesType(c) != parenthesesStack.pop())) return false;
+        }
+
+        return parenthesesStack.isEmpty();
+    }
+
+    private static boolean isOpeningParentheses(char c) {
+        return '(' == c || '{' == c || '[' == c;
+    }
+
+    private static boolean isClosingParentheses(char c) {
+        return ')' == c || '}' == c || ']' == c;
+    }
+
+    private static int getParenthesesType(char c) {
+        return switch (c) {
+            case '(', ')' -> PARENTHESES;
+            case '{', '}' -> BRACKET;
+            case '[', ']' -> SQUARE;
+            default -> NONE;
+        };
     }
 
 }
--- tmog-002/src/test/java/org/tfoc/SolutionTest.java
+++ tmog-002/src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,37 @@
 package org.tfoc;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void shouldBeValid() {
+        // given
+        String input = "([(){}])";
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        // when
+        boolean result = Solution.isValid(input);
+
+        // then
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldNotBeValid() {
+        // given
+        String input = "([)]";
+
+        // when
+        boolean result = Solution.isValid(input);
+
+        // then
+        assertFalse(result);
     }
 
 }
