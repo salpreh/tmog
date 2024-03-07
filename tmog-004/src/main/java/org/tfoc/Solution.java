f31d4a46a5087f886e7e98b22464c089e8aaec64
Salva Pérez <sperher@mercadona.es>
2024-03-07 08:18:44 +0100

    tmog4 solution

--- tmog-004/src/main/java/org/tfoc/Solution.java
+++ tmog-004/src/main/java/org/tfoc/Solution.java
@@ -5,11 +5,38 @@ package org.tfoc;
  */
 public class Solution {
 
-    public ListNode mergeTwoLists(
-            ListNode list1,
-            ListNode list2) {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) return null;
 
-        return null;
+        ListNode mergedRoot = new ListNode();
+        ListNode currentMerged = mergedRoot;
+        ListNode currentNode1 = list1;
+        ListNode currentNode2 = list2;
+        while (currentNode1 != null && currentNode2 != null) {
+            if (currentNode1.val > currentNode2.val){
+                currentMerged.next = new ListNode(currentNode2.val);
+                currentNode2 = currentNode2.next;
+            } else {
+                currentMerged.next = new ListNode(currentNode1.val);
+                currentNode1 = currentNode1.next;
+            }
+
+            currentMerged = currentMerged.next;
+        }
+
+        while (currentNode1 != null) {
+            currentMerged.next = new ListNode(currentNode1.val);
+            currentMerged = currentMerged.next;
+            currentNode1 = currentNode1.next;
+        }
+
+        while (currentNode2 != null) {
+            currentMerged.next = new ListNode(currentNode2.val);
+            currentMerged = currentMerged.next;
+            currentNode2 = currentNode2.next;
+        }
+
+        return mergedRoot.next;
     }
 
 }
--- tmog-004/src/test/java/org/tfoc/SolutionTest.java
+++ tmog-004/src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,73 @@
 package org.tfoc;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void shouldSortWhen2Lists() {
+        // given
+        ListNode list1 = createList1();
+        ListNode list2 = createList2();
+        int[] expected = new int[]{1,1,2,3,4,7};
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        // when
+        ListNode result = Solution.mergeTwoLists(list1, list2);
+
+        // then
+        assertListResult(expected, result);
+    }
+
+    @Test
+    void shouldSortWhen1List() {
+        // given
+        ListNode list1 = createList1();
+        int[] expected = new int[]{1,2,4};
+
+        // when
+        ListNode result = Solution.mergeTwoLists(list1, null);
+
+        // then
+        assertListResult(expected, result);
+    }
+
+    @Test
+    void shouldSortWhenEmptyLists() {
+        // when
+        ListNode result = Solution.mergeTwoLists(null, null);
+
+        // then
+        assertNull(result);
+    }
+
+    private ListNode createList1() {
+        ListNode rootNode = new ListNode(1);
+        rootNode.next = new ListNode(2);
+        rootNode.next.next = new ListNode(4);
+
+        return rootNode;
+    }
+
+    private ListNode createList2() {
+        ListNode rootNode = new ListNode(1);
+        rootNode.next = new ListNode(3);
+        rootNode.next.next = new ListNode(7);
+
+        return rootNode;
+    }
+
+    private static void assertListResult(int[] expected, ListNode result) {
+        ListNode resultNode = result;
+        for (int j : expected) {
+            Assertions.assertEquals(j, resultNode.val);
+            resultNode = resultNode.next;
+        }
     }
 
 }
