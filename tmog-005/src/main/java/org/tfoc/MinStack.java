41918e14a11f7f913a7f177452617e2477c160e4
Salva Pérez <sperher@mercadona.es>
2024-03-09 14:34:11 +0100

    tmog5 problem solution

--- tmog-005/src/main/java/org/tfoc/MinStack.java
+++ tmog-005/src/main/java/org/tfoc/MinStack.java
@@ -7,29 +7,63 @@ package org.tfoc;
  * obj.pop()
  * int param_3 = obj.top()
  * int param_4 = obj.getMin()
+ *
+ * CONSTRAINTS: Methods pop, top and getMin operations will always be called on non-empty stacks.
  */
 public class MinStack {
 
-    public MinStack() {
+    private static final int MIN_CAPACITY = 10;
 
+    private int[] stack;
+    private int topIdx = -1;
+    private int minIdx;
+
+    public MinStack() {
+        stack = new int[MIN_CAPACITY];
     }
 
     public void push(int val) {
+        if (topIdx == stack.length - 1) {
+            resize();
+        }
 
+        stack[++topIdx] = val;
+
+        if (val < stack[minIdx]) {
+            minIdx = topIdx;
+        }
     }
 
     public void pop() {
-
+        topIdx--;
+        if (topIdx < minIdx) recalculateMin();
     }
 
     public int top() {
-
-        return 0;
+        return stack[topIdx];
     }
 
     public int getMin() {
+        return stack[minIdx];
+    }
 
-        return 0;
+    private void resize() {
+        int[] newStack = new int[stack.length << 1];
+        for (int i = 0; i < stack.length; i++) {
+            newStack[i] = stack[i];
+        }
+
+        stack = newStack;
+    }
+
+    private void recalculateMin() {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= topIdx; i++) {
+            if (min >= stack[i]) {
+                min = stack[i];
+                minIdx = i;
+            }
+        }
     }
 
 }
--- tmog-005/src/test/java/org/tfoc/MinStackTest.java
+++ tmog-005/src/test/java/org/tfoc/MinStackTest.java
@@ -1,31 +1,56 @@
 package org.tfoc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Stack;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class MinStackTest {
 
     @Test
-    void testMinStack() {
+    void shouldChainOperationsCorrectly() {
+        // given
+        MinStack stack = new MinStack();
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        // then
+        stack.push(-2);
+        stack.push(0);
+        stack.push(-3);
+        assertEquals(-3, stack.getMin());
+
+        stack.pop();
+        assertEquals(0, stack.top());
+        assertEquals(-2, stack.getMin());
     }
 
     @Test
-    void push() {
-    }
+    void shouldChainOperationsCorrectlyWhenEmptyingStack() {
+        // given
+        MinStack stack = new MinStack();
 
-    @Test
-    void pop() {
-    }
+        stack.push(2147483646);
+        stack.push(2147483646);
+        stack.push(2147483647);
 
-    @Test
-    void top() {
-    }
+        assertEquals(2147483647, stack.top());
+        stack.pop();
+        assertEquals(2147483646, stack.getMin());
+        stack.pop();
+        assertEquals(2147483646, stack.getMin());
 
-    @Test
-    void getMin() {
-    }
+        stack.pop();
+        stack.push(2147483647);
+        assertEquals(2147483647, stack.top());
+        assertEquals(2147483647, stack.getMin());
 
+        stack.push(-2147483648);
+        assertEquals(-2147483648, stack.top());
+        assertEquals(-2147483648, stack.getMin());
+
+        stack.pop();
+        assertEquals(2147483647, stack.getMin());
+    }
 }
